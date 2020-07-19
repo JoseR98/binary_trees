78b72504bc56e22750a0bfc9186e807b855b7dbf
sebastianbm9507 <juan.bueno01@usc.edu.co>
2020-07-19 22:07:34 +0000

    Create new node, Update header file

--- binary_trees.h
+++ binary_trees.h
@@ -1,6 +1,9 @@
 #ifndef BINARY_T_H
 #define BINARY_T_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -18,5 +21,8 @@ struct binary_tree_s
 };
 
 typedef struct binary_tree_s binary_tree_t;
+
+void binary_tree_print(const binary_tree_t *);
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
 #endif
