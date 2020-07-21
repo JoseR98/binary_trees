6e5af5e442da874c836a84c712a87387afa77fd5
sebastianbm9507 <juan.bueno01@usc.edu.co>
2020-07-21 16:50:17 +0000

    Fix identation

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -2,11 +2,11 @@
 /**
  * binary_tree_size - function that measures the size of a binary tree
  * @tree:  pointer to the root node of the tree to measure the size.
- * Return: size of the tree.
+ * Return: size of the tree
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return (0);
-    return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
+	if (tree == NULL)
+		return (0);
+	return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
 }
