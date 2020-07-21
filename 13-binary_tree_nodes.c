9f0aa1ffafcb0ccc78f3adb78068352b4c2bd1b4
sebastianbm9507 <juan.bueno01@usc.edu.co>
2020-07-21 16:51:34 +0000

    Fix identation

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -11,9 +11,9 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 	nodes = 0;
 	if (tree == NULL)
 		return (0);
-    if (tree->left != NULL && tree->right != NULL)
-    /* minus one parent */
-        nodes -= 1;
+	if (tree->left != NULL && tree->right != NULL)
+	/* minus one parent */
+		nodes -= 1;
 	if (tree->left != NULL)
 		nodes += 1 + binary_tree_nodes(tree->left);
 	if (tree->right != NULL)
